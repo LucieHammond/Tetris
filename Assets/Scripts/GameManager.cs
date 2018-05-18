33114633c7601f5557a5555b6a1d78e82ac9ace9
Lucie Hammond <lucie.hammond@student.ecp.fr>
2018-05-18 12:03:06 +0200

    Implement the Tetris Random Generator

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Application;
+
+public class GameManager : MonoBehaviour
+{
+
+	public GameObject[] tetriminos;
+	public Vector3 spawnPoint = new Vector3(3, 22, 0);
+	private Queue<GameObject> nextPieces = new Queue<GameObject>();
+	private System.Random random = new System.Random();
+
+	// Use this for initialization
+	private void Start () {
+		spawnPoint = transform.position + spawnPoint;
+		SpawnTetrimino();
+	}
+    
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+
+    private void SpawnTetrimino()
+	{
+		if (nextPieces.Count <= 3)
+		{
+			RandomGenerator();
+		}
+		GameObject tetrimino = nextPieces.Dequeue();
+        Instantiate(tetrimino, spawnPoint, Quaternion.identity);
+	}
+    
+	private void RandomGenerator()
+	{
+		GameObject[] nextBag = random.Shuffle(tetriminos);
+		for (int i = 0; i < nextBag.Length; i++)
+			nextPieces.Enqueue(nextBag[i]);
+	}
+}
---
+++ Assets/Scripts/RandomExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Application
+{
+	static class RandomExtensions
+    {
+        public static T[] Shuffle<T>(this Random rng, T[] array)
+        {
+			T[] result = array.Clone() as T[];
+            int n = result.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = result[n];
+                result[n] = result[k];
+                result[k] = temp;
+            }
+			return result;
+        }
+    }
+}
