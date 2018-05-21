1a51985eaac6401848dd58814a893e0dff2169ff
Lucie Hammond <lucie.hammond@student.ecp.fr>
2018-05-21 03:50:34 +0200

    Add Splash Screen with Tetris logo

---
+++ Assets/Scripts/WaitingBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitingBehaviour : MonoBehaviour {
+
+	public float waitingTime;
+	public RectTransform loadingBar;
+    
+	void Start () {
+		StartCoroutine(WaitAndLoadScene());
+		loadingBar.sizeDelta = new Vector2(0, loadingBar.sizeDelta.y);
+	}
+
+    IEnumerator WaitAndLoadScene()
+	{
+		float delta = waitingTime / 100;
+		for (int i = 1; i <= 100; i++){
+			loadingBar.sizeDelta = new Vector2(i * 3, loadingBar.sizeDelta.y);
+			yield return new WaitForSeconds(delta);
+		}
+		SceneManager.LoadScene("GameScene");
+	}
+
+}
