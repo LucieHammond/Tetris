f9a1c601105a1bc4b6c02924db6cb2a0a0f04cc2
Lucie Hammond <lucie.hammond@student.ecp.fr>
2018-05-21 05:52:54 +0200

    Add Menu scene with 5 buttons

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+
+	public string sceneName = "";
+	public bool disabled;
+
+	private void Start()
+	{
+		if (disabled)
+		{
+			GetComponent<Button>().interactable = false;
+		}
+	}
+
+	public void OnClick()
+	{
+		if (sceneName != "") 
+		    SceneManager.LoadScene(sceneName);
+	}
+}
